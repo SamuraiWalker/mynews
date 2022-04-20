34a5aaa92320b21463468e4815c94d73b0b0d87b
Samurai_Walker <T6.0sa7i@gmail.com>
2022-04-20 11:26:14 +0000

    For PHP_Laravel12_Task1~3

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,22 @@
 */
 
 //PHP_Laravel09_Task3
-Route::get('xxx','AAAController/@bbb');
+Route::get('xxx','AAAController\@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     //PHP_Laravel09_Task4
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    //PHP_Laravel12_Task2
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    //PHP_Laravel12_Task3
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    // Route::get('news/create', 'Admin/NewsController@add')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
