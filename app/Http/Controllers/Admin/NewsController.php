8d0250981722ee8d26bdd5bb7eb3088ad133a542
Samurai_Walker <T6.0sa7i@gmail.com>
2022-04-24 07:20:50 +0000

    For PHP_Larabel14_Task4~8

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
+
 class NewsController extends Controller
 {
     public function add()
@@ -15,8 +18,34 @@ class NewsController extends Controller
   // 以下を追記
     public function create(Request $request)
     {
+        // 以下を追記
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存しえ、$news->image_pathに画像のパスを保存する
+    
         // admin/news/createにリダイレクトする
-        return redirect('admin.news.create');
+        
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);            
+        } else {
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
+        return redirect('admin/news/create');
     }
 }
         
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profiles;
+
 class ProfileController extends Controller
 {
     //PHP/Laravel08 ControllerとRoutingとの関係について 課題５
@@ -12,16 +14,35 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    public function create()
+    
+    public function create(Request $request)
     {
-        return redirect('admin.profile.create');
+    // 以下を追記
+        // Varidationを行う
+        $this->validate($request, Profiles::$rules);
+        
+        $profiles = new Profiles;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+        
+        // admin/news/createにリダイレクトする
+        return redirect('admin/profile/create');
     }
+    
     public function edit()
     {
         return view('admin.profile.edit');
     }
     public function update()
     {
-        return redirect('admin.profile.edit');
+        return redirect('admin/profile/edit');
     }
+    
+    
 }
